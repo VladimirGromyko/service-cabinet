472243a4465a2540b7da0b56d47d3f14da99889e
VladimirGromyko <vl-mailbox@mail.ru>
2024-08-09 11:08:17 +0300

    1. Added expand block,

--- src/components/ServiceJournal/InfoPage/InformationPage.tsx
+++ src/components/ServiceJournal/InfoPage/InformationPage.tsx
@@ -1,12 +1,12 @@
-import { useAppSelector } from "@/store/hooks";
+import {useAppSelector} from "@/store/hooks";
 import s from "./infoPage.module.scss"
-import { ButtonsInfoPage } from "@/components/ServiceJournal/InfoPage/Butttons/ButtonsInfoPage";
-import { JournalHeader } from "@/components/ServiceJournal/JournalHeader/JournalHeader";
+import {ButtonsInfoPage} from "@/components/ServiceJournal/InfoPage/Butttons/ButtonsInfoPage";
+import {JournalHeader} from "@/components/ServiceJournal/JournalHeader/JournalHeader";
 import React, {useState} from "react";
 import {dateComparisonValue, timePassedString} from "@/utils/utils";
-import { StatusImg } from "@/components/utils/StatusImg/StatusImg";
-import { Button, Flex } from "antd";
-import { DownOutlined } from '@ant-design/icons';
+import {StatusImg} from "@/components/utils/StatusImg/StatusImg";
+import {Button, Flex} from "antd";
+import {DownOutlined, UpOutlined} from '@ant-design/icons';
 
 export const InformationPage = () => {
     const longString = useAppSelector((state) => {
@@ -15,41 +15,52 @@ export const InformationPage = () => {
     const timePassed = dateComparisonValue({
         current: new Date() as string, comparable: longString.term
     });
+    const [expand, setExpand] = useState(false)
     return (
         <div className={s.infoPageWrapper}>
             <JournalHeader/>
             <div className={s.infoPageSection}>
                 <ButtonsInfoPage/>
-                <span className={s.infoPageSpace}>
-                  <div className={s.mainInfo}>
-                      <div className={s.fieldName}>
-                          Обращение № {longString.number} от {longString.creationDate}
-                      </div>
-                      <div className={s.infoTheme}>{longString.theme}</div>
-                      <div>{longString.description}</div>
-                  </div>
-                  <div className={s.extraInfo}>
-                      <div className={s.statusImg}>
-                          <StatusImg status={longString.status}/>
-                          <span>{longString.status}</span>
-                      </div>
-                      <div className={s.deadline}>
-                          <div className={s.fieldName}>Крайний срок</div>
-                          <div>{longString.term} - {timePassedString(timePassed)} назад</div>
-                      </div>
-                      <div>
-                          <div className={s.fieldName}>Решение</div>
-                          <div>{longString.solution}</div>
-                      </div>
-                  </div>
+
+                <span className={s.infoPageAllSpace}>
+                    <span className={s.infoPageSpace}>
+                        <div className={s.mainInfo}>
+                          <div className={s.fieldName}>
+                              Обращение № {longString.number} от {longString.creationDate}
+                          </div>
+                          <div className={s.infoTheme}>{longString.theme}</div>
+                          <div>{longString.description}</div>
+                        </div>
+                        <div className={s.extraInfo}>
+                          <div className={s.statusImg}>
+                              <StatusImg status={longString.status}/>
+                              <span>{longString.status}</span>
+                          </div>
+                          <div className={s.deadline}>
+                              <div className={s.fieldName}>Крайний срок</div>
+                              <div>{longString.term} - {timePassedString(timePassed)} назад</div>
+                          </div>
+                          <div>
+                              <div className={s.fieldName}>Решение</div>
+                              <div>{longString.solution}</div>
+                          </div>
+                        </div>
+                    </span>
+                    {expand && (
+                        <span>
+                            <div className={s.fieldName}>Дополнительная информация</div>
+                            <div>Услуга - {longString.service}</div>
+                            <div>Состав услуги - {longString.serviceComposition}</div>
+                        </span>)}
                 </span>
+
                 <Flex justify="center">
                     <Button
                         shape="circle"
-                        icon={<DownOutlined />}
+                        icon={expand ? <UpOutlined /> : <DownOutlined />}
                         size="large"
                         className={s.buttonDown}
-                        onClick={() => console.log("Hello")}
+                        onClick={() => setExpand(!expand)}
                     />
                 </Flex>
                 <span className={s.additionally}>
